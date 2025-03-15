46ad08eef72889d9fe33d70c82871d0e1d1e07d4
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-03-16 00:26:40 +0100

    Otimizing view for recep the form information

--- core/views.py
+++ core/views.py
@@ -3,8 +3,15 @@ from .forms import QRCodeForm
 
 
 def generete_qr_code(request):
-    form = QRCodeForm()
-    context = {
-        'form': form
-    }
-    return render(request, 'generete_qr_code.html', context)
+    if request.method == 'POST':
+        form = QRCodeForm(request.POST)
+        if form.is_valid():
+            res_name = form.cleaned_data['restaurant_name']
+            url = form.cleaned_data['url']
+        return
+    elif request.method == 'GET':
+        form = QRCodeForm()
+        context = {
+            'form': form
+        }
+        return render(request, 'generete_qr_code.html', context)
