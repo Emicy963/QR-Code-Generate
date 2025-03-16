dfccf0f8338d37b2655eb1999ccfc04786964fa1
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-03-16 01:23:11 +0100

    Get for QR Code generate and put in media path, next get it on media path and render to template

--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .forms import QRCodeForm
 import qrcode
+import os
+from django.conf import settings
 
 
 def generete_qr_code(request):
@@ -13,9 +15,16 @@ def generete_qr_code(request):
             # Generate QR Code
             qr = qrcode.make(url)
             file_name = res_name.replace(" ", "_").lower() + '_menu.png'
-            qr.save(file_name)
+            file_path = os.path.join(settings.MEDIA_ROOT, file_name) #../media/exemplo_menu.png
+            qr.save(file_path)
+
+            # Create image url
+            qr_url = os.path.join(settings.MEDIA_URL, file_name)
+            print('media url-', qr_url)
+
             context = {
-                'res_name': res_name.title()
+                'res_name': res_name.title(),
+                'qr_url': qr_url,
             }
             return render(request, 'qr_result.html', context)
     elif request.method == 'GET':
