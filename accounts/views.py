1cd74074f2621aff0f12c0374cdb378c4d614792
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-04-17 00:18:24 +0100

    App for register user

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.contrib import messages
+from django.contrib.auth import login, authenticate, logout
 
-# Create your views here.
+def register_view(request):
+    if request.method=='GET':
+        return render(request, 'register.html')
+    else:
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        password1 = request.POST.get('password1')
+
+        if User.objects.filter(username=username).exists() or User.objects.filter(email=email).exists():
+            messages.add_message(request, constants.ERROR, 'Username or Email already exist!')
+            return redirect('register')
+        elif password!=password1:
+            messages.add_message(request, constants.ERROR, "Password don't match!")
+            return redirect('register')
+        
+        try:
+            user = User.objects.create_user(
+                username=username,
+                email=email,
+                password=password
+            )
+            
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.add_message(request, constants.SUCCESS, 'Sucess in Create user!')
+                return redirect('generete_qr_code')
+            else:
+                messages.add_message(request, constants.ERROR, 'Authenticated error!')
+                return redirect('register')
+        except Exception as err:
+            messages.add_message(request, constants.ERROR, f'Error: {str(err)}')
+            return redirect('register')
